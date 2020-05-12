e7405be7ebcdae079d8f76fc4c29314a66a1b8ed
Hano-Guru <bashiromar94@gmail.com>
2020-05-12 15:56:22 +0300

    import db

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hanad:hano123@localhost/khalid'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+        debug = os.environ.get("DEBUG")
+
+
+class TestConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hanad:hano123@localhost/khalid'
+    pass
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hanad:hano123@localhost/khalid'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
